b7f5643fe83bf5059192ad32940fe6a627d53527
Name <email@example.ru>
2023-06-16 09:14:01 +0300

    1st

--- HomeApp/App.xaml.cs
+++ HomeApp/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HomeApp.Pages;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -10,7 +11,7 @@ namespace HomeApp
         {
             InitializeComponent();
 
-            MainPage = new SpanPage();
+            MainPage = new NewDevicePage();
         }
 
         protected override void OnStart()
---
+++ HomeApp/Pages/LoginPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+using Xamarin.Forms;
+
+namespace HomeApp.Pages
+{
+    public partial class LoginPage : ContentPage
+    {
+        // Константа для текста кнопки
+        public const string BUTTON_TEXT = "Войти";
+        // Переменная счетчика
+        public static int loginCouner = 0;
+        public LoginPage()
+        {
+            InitializeComponent();
+
+            if (Device.RuntimePlatform == Device.UWP)
+            {
+                loginButton.CornerRadius = 0;
+            }
+        }
+
+        /// <summary>
+        /// По клику обрабатываем счётчик и выводим разные сообщения
+        /// </summary>
+        private void Login_Click(object sender, EventArgs e)
+        {
+            if (loginCouner == 0)
+            {
+                // Если первая попытка - просто меняем сообщения
+                loginButton.Text = $"Выполняется вход..";
+            }
+            else if (loginCouner > 5) // Слишком много попыток - показываем ошибку
+            {
+                // Деактивируем кнопку
+                loginButton.IsEnabled = false;
+
+                // Получаем последний дочерний элемент, используя свойство Children, затем выполняем распаковку
+                var infoMessage = (Label)stackLayout.Children.Last();
+                // Задаем текст элемента
+                infoMessage.Text = "Слишком много попыток! Попробуйте позже";
+                // задаем красный цвет сообщения
+                infoMessage.TextColor = Color.FromRgb(255, 0, 0);
+
+            }
+            else
+            {
+                // Изменяем текст кнопки и показываем количество попыток входа
+                loginButton.Text = $"Выполняется вход...   Попыток входа: {loginCouner}";
+            }
+
+            // Увеличиваем счетчик
+            loginCouner += 1;
+        }
+    }
+}
